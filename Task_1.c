9359d47a3d444dc07e1f9fb3852a118b99cb464f
Mykyta Ivaniuk <46998394+nikash97@users.noreply.github.com>
2020-05-22 18:37:08 +0200

    Comments added

--- Task_1.c
+++ Task_1.c
@@ -2,18 +2,19 @@
 #include <inttypes.h>
 #include <math.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]){		//function main with parameters to accept command-line arguments
     int N, K, S;                        //initialize variables: amount of steps, K-th number, result
     N = strtoimax(argv[1], NULL, 0);    //read command-line arguments to integers
     K = strtoimax(argv[2], NULL, 0);
-    S = fmod(N,(K+1));                  //S will temporarily contain remainder of division
+    S = fmod(N,(K+1));                  //S will temporarily contain remainder of N/(K+1) division, will be needed further
 
-    if (S==0)                           //if remainder is 0 (S is whole number), return result of division
+	//The description of a task was not very clear for me, so I noticed a pattern using examples
+	
+	//If remainder S is 0 (result of N/(K+1) division is a whole number), then return result of division
+    if (S==0)
         S = N/(K+1);
-    else                                //otherwise return the integer part of S + 1
+    else                                //otherwise return the integer part of division + 1
         S = floor(N/(K+1))+1;
 
     printf("%d", S);
-    return S;
 }
-
--- Task_2.c
+++ Task_2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-int main(int argc, char *argv[]){
-    int N, D, S;                        //initialize variables: amount of steps, K-th number, result
+int main(int argc, char *argv[]){		//function main with parameters to accept command-line arguments
+    int N, D, L;                        //initialize: amount of spans, length of a single span, total length of wires
     N = strtoimax(argv[1], NULL, 0);    //read command-line arguments to integers
     D = strtoimax(argv[2], NULL, 0);
 
-    S = 0.5*(N*N+N)*D;                  //To explain why
+    L = 0.5*(N*N+N)*D;      //The total length of wires can be calculated as total number of spans multiplied by their
+							//length D. The number of spans, according to the bridge's "blueprint" can be defined as 
+							//sum of arithmetic progression, where first element is 1, last N, and the amount of
+							//terms in progression also N. Sn = (a1+aN)*N/2
 
-    printf("%d", S);
+    printf("%d", L);
 }
